9328f0fcc82c3a3ec8b67dd04eb1a9789fd86dda
krlosMata <krlosmata@gmail.com>
2019-03-12 09:57:17 +0100

    update index.js

--- js/index.js
+++ js/index.js
@@ -433,15 +433,12 @@ function getNotificationsToaster() {
 
 // Ask to notification server for last 10 notifications
 function getNotifications() {
-  console.log(1);
   if (g.notifications == null) {
     g.notifications = {};
     g.notificationLastId = 0;
   }
-  console.log(2);
   return g.id.getNotifications(0, g.notificationLastId)
     .then((res) => {
-      console.log(3);
       const notifications = res.data.notifications;
       if (notifications == null) {
         return false;
@@ -457,7 +454,6 @@ function getNotifications() {
       return true;
     })
     .catch((err) => {
-      console.log(4);
       toastr.error(err);
       console.error(err.response.data);
       let check = document.getElementById('notification-background');
@@ -508,8 +504,6 @@ function toggleNotificationsBackground() {
 function deleteNotifications(){
 
 }
-<<<<<<< a2f0e7f35ee1a7a9c7e6f962b6663e158293f18a
-<<<<<<< bf306719f6107b0c53d932ad3a9649b94820749f
 
 // Verify Proof of Claim Full
 function verifyProofClaimFull() {
@@ -532,9 +526,6 @@ function verifyProofClaimFull() {
   }
   document.getElementById("proofClaimVerification-result").innerHTML = html;
 }
-=======
-<<<<<<< 5294db02cd101cd7ae0a175dbfb43435945bdfd5
-=======
 
 // const hello = await axiosGetDebug(`${loginUrl}/auth/hello`, { headers: { Authorization: `Bearer ${token.data.token}` } });
 // Background worker code
@@ -563,7 +554,3 @@ function verifyProofClaimFull() {
 // const proofKeyTest = JSON.parse(document.getElementById('proofClaimOperationalKey-result').innerHTML);
 // const login = await id.loginNotificationServer(proofEthName, kc, id.keyOperationalPub, proofKSign);
 // console.log(id.notificationServer);
->>>>>>> Add steps full flow. Finish first step
->>>>>>> Add steps full flow. Finish first step
-=======
->>>>>>> update variables
